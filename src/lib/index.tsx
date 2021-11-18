868dda8a467627b1765eb77616771064478952f7
adiathasan <adiathasan.me@gmail.com>
2021-11-18 21:16:06 +0600

    Added naming options for more solid errors and indications

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -4,15 +4,29 @@ interface StoreProviderProps {
 	children?: React.ReactNode;
 }
 
+export interface StoreOptions {
+	useStoreName?: string;
+	useDispatchName?: string;
+	providerName?: string;
+}
+
 /**
  *
  * @param init -> initial state
  * @param reducer -> reducer function
+ * @param {StoreOptions}
  * @returns -> [StoreProvider -> wrap it to access the hooks [store, dispatch], useStore -> access the store, useDispatch -> dispath actions]
  */
-export const createStore = <S, A>(init: S, reducer: (state: S, action: A) => S) => {
+export const createStore = <S, A>(
+	init: S,
+	reducer: (state: S, action: A) => S,
+	{ useStoreName = 'useStore', useDispatchName = 'useDispatch', providerName = 'Provider' }: StoreOptions
+) => {
 	const StoreContext = React.createContext<any>(null);
 	const StoreDispatchContext = React.createContext<any>(null);
+
+	StoreContext.displayName = providerName;
+	StoreDispatchContext.displayName = `${providerName}Dispatch`;
 
 	/**
 	 *
@@ -39,7 +53,7 @@ export const createStore = <S, A>(init: S, reducer: (state: S, action: A) => S) 
 		const store = React.useContext<S>(StoreContext);
 
 		if (!store) {
-			throw new Error('createStore->fn: useStore must be consumed (used) within a Provider');
+			throw new Error(`${useStoreName} must be consumed (used) within a ${providerName}`);
 		}
 
 		return store;
@@ -53,7 +67,7 @@ export const createStore = <S, A>(init: S, reducer: (state: S, action: A) => S) 
 		const dispatch = React.useContext<React.Dispatch<A>>(StoreDispatchContext);
 
 		if (!dispatch) {
-			throw new Error('createstore->fn: useDispatch must be consumed (used) within a Provider');
+			throw new Error(`${useDispatchName} must be consumed (used) within a ${providerName}`);
 		}
 
 		return dispatch;
